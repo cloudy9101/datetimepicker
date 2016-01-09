b08c3048bdad35fb5e079a2ba93784d81510fe83
cloudy9101 <cloudy9101@126.com>
2016-01-09 22:53:27 +0800

    add zero and some css

--- app/datetime_picker/datetime_bar.jsx
+++ app/datetime_picker/datetime_bar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class DateTimeBar extends React.Component {
+  addZero(n) {
+    return n < 10 ? '0' + n : n;
+  }
   render() {
     const date = this.props.selectedDate;
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -11,10 +14,10 @@ class DateTimeBar extends React.Component {
         <div className="next-month" onClick={this.props.nextMonth}><a href='javascript:;'><i className="material-icons">chevron_right</i></a></div>
         <div className="time-bar">
           <span> {weekdays[date.getDay()]}</span>
-          <span> {months[date.getMonth()]}</span>
-          <span> {date.getDate()}</span>
+          <span> {this.addZero(months[date.getMonth()])}</span>
+          <span> {this.addZero(date.getDate())}</span>
           <span onClick={this.props.showYearSelector}> <a href='javascript:;'>{date.getFullYear()}</a></span>
-          <span onClick={this.props.showTimeSelector}> <a href='javascript:;'>{date.getHours()}:{date.getMinutes()}</a></span>
+          <span onClick={this.props.showTimeSelector}> <a href='javascript:;'>{this.addZero(date.getHours())}:{this.addZero(date.getMinutes())}</a></span>
         </div>
       </div>
     );
